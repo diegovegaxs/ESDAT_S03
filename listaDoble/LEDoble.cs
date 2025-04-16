eea6eb844afebd8a64bfe79bc1edfaeea06dc999
diegovegaxs <diegovegaxs@gmail.com>
2025-04-15 23:28:27 -0500

    Tarea Agregar metodos a LEDoble

--- listaDoble/LEDoble.cs
+++ listaDoble/LEDoble.cs
@@ -10,19 +10,103 @@ namespace listaDoble {
         Nodo Ultimo { get; set; }
 
         //agrega un valor al inicio de LE Doble
-        public void AgregaPrimero(int valor) {
-
+        public void AgregaPrimero(int valor)
+        {
+            Nodo nuevo = new Nodo(valor);
+            if (this.Primero == null)
+            {
+                this.Primero = nuevo;
+            }
+            else
+            {
+                Nodo prim = this.Primero;
+                nuevo.Sig = prim;
+                nuevo.Ant = null;
+                nuevo.Sig = prim;
+                this.Primero = nuevo;
+            }
         }
 
         //agrega un valor al final de LE Doble
-        public void AgregaFinal(int valor) {
-            
+        public void AgregaFinal(int valor) 
+        {
+            Nodo final = new Nodo(valor);
+            if (this.Primero == null)
+            {
+                this.Primero = final;
+                this.Ultimo = final;
+            }
+            else
+            {
+                this.Ultimo.Sig = final;
+                final.Ant = this.Ultimo;
+                this.Ultimo = final;
+            }
         }
 
         //ve valores separados por comas de la LEDoble
-        public override string ToString() {
-            
-            return "";
+        public override string ToString() 
+        {
+            string listaVag = "";
+            Nodo tmp = this.Primero;
+            while (tmp != null)
+            {
+                listaVag += tmp.Dato + ", ";
+                tmp = tmp.Sig;
+            }
+            return listaVag;
+        }
+
+        public int Largo()
+        {
+            if (this.Primero == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int cant = 0;
+                Nodo tmp = this.Primero;
+                while (tmp != null)
+                {
+                    cant++;
+                    tmp = tmp.Sig;
+                }
+                return cant;
+            }
+        }
+
+        public int GetValor(int pos)
+        {
+            int indice = 0;
+            Nodo tmp = this.Primero;
+            while (tmp != null)
+            {
+                if (indice == pos)
+                {
+                    return tmp.Dato;
+                }
+                indice++;
+                tmp = tmp.Sig;
+            }
+            return int.MinValue;
+        }
+        public void EliminarPrimero()
+        {
+            if (this.Primero == null) 
+            {
+                return;
+            }
+            else if (this.Primero == this.Ultimo)
+            {
+                this.Primero = null;
+                this.Ultimo = null;
+            }
+            else
+            {
+                this.Primero = this.Primero.Sig;
+                this.Primero.Ant = null;
+            }
         }
     }
 }
--- listaDoble/Program.cs
+++ listaDoble/Program.cs
@@ -3,6 +3,14 @@
 LEDoble l = new LEDoble();
 l.AgregaFinal(20);
 l.AgregaFinal(10);
-l.AgregaPrimero(2); 
+l.AgregaPrimero(54); 
 Console.WriteLine(l.ToString());    //2,20,10
+l.AgregaPrimero(3);
+l.AgregaFinal(4);
+l.AgregaFinal(18);
+Console.WriteLine(l.ToString());
+Console.WriteLine(l.Largo());
+Console.WriteLine(l.GetValor(2));
+l.EliminarPrimero();
+Console.WriteLine(l.ToString());
 Console.WriteLine();
